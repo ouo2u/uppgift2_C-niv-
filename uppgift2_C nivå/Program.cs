3277be7adb219c239dd141bb389c0b3d42266217
ouo2u <111962660+ouo2u@users.noreply.github.com>
2023-02-17 10:44:54 +0100

    Update Program.cs

--- uppgift2_C nivå/Program.cs
+++ uppgift2_C nivå/Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace uppgiftett_Enivå
+{
+    class program
+    {
+        static int celsiusTofahrenheit(int c)
+        {
+            int f = ((c * 9) / 5) + 32;
+            return f;
+        }
+        static double FarenheitTocelsius(double k)
+        {
+            
+            return (k-32/1.8);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv 1 eller 2 beror på vilken omvandling du vill göra");
+            Console.WriteLine("1.Omvandla från celsius till fahrenheit");
+            Console.WriteLine("2.Omvandla från fahrenheit till celsius");
+            string val=Console.ReadLine();
+
+            switch (val)
+            {
+                case "1":
+                    {
+                        Console.WriteLine("skriv in grader i celsius");
+                        int celsius = Convert.ToInt32(Console.ReadLine());
+                        int fahrenheit = celsiusTofahrenheit(celsius);
+                        Console.WriteLine("Det blir " + fahrenheit + " grader i fahrenheit.");
+                    }
+
+               break;
+                case "2":
+                    {
+                        Console.WriteLine("skriv in grader i fahrenheit");
+                        double k = Convert.ToDouble(Console.ReadLine());
+                        double celsius = FarenheitTocelsius(k);
+                        Console.WriteLine("Det blir " + celsius + " grader i celsius.");
+
+                    }
+                    break;
+            }
+
+
+
+        }
+     
+    }
+}
